767811f9f87481f6397ac83b1cd32871f5f03dbd
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-18 17:34:56 -0600

    deleted commented out code

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,11 @@
-import MrKrabsCard from "./components/MrKrabsCard";
-import PatrickStarCard from "./components/PatrickStarCard";
-import SpongebobCard from "./components/SpongeBobCard"
-import SquidwardCard from "./components/SquidwardCard";
-// import friends from "./friends.json";
 import Home from "./pages/home"
 
 function App() {
   return (
     <div>
       <Home></Home>
-      {/* <SpongebobCard info= {friends[0]}></SpongebobCard> 
-      <MrKrabsCard info= {friends[1]}></MrKrabsCard>
-      <SquidwardCard info= {friends[2]}></SquidwardCard>
-      <PatrickStarCard info= {friends[3]}></PatrickStarCard> */}
     </div>
   );
-}
+};
 
 export default App;
